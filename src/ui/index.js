238fedc86b95dd51cdf5e97f90138564b0a680ee
Novout <novout@hotmail.com>
2021-02-07 03:11:35 -0300

    refactor: ui default color

--- src/ui/index.js
+++ src/ui/index.js
@@ -10,22 +10,25 @@ export const CreateUI = (app, player) => {
 }
 
 export const CreateInventoryBar = (app, player) => {
-  let alpha = new filters.AlphaFilter(0.5);
+  const alpha_main = new filters.AlphaFilter(0.5);
+  const alpha_item = new filters.AlphaFilter(0.15);
+
   let inventory = new Container();
-  inventory.filters = [alpha];
   inventory.position.set(player.x - 75, player.y - 100);
   app.stage.addChild(inventory);
 
   let bar = new Graphics();
-  bar.beginFill(0x000000);
+  bar.beginFill(0x66BD99);
   bar.drawRect(0, 0, 256, 64);
   bar.endFill();
+  bar.filters = [alpha_main];
   inventory.addChild(bar);
 
   let item = new Graphics();
-  item.beginFill(0xFF3300);
+  item.beginFill(0xFFFFFF);
   item.drawRect(0, 0, 64, 64);
   item.endFill();
+  item.filters = [alpha_item];
   inventory.addChild(item);
 
   inventory.outer = item;
