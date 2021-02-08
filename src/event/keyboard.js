c0394dcad3ca9e17e99e324690e48ce6682d384e
Novout <novout@hotmail.com>
2021-02-08 05:19:37 -0300

    refactor: player state

--- src/event/keyboard.js
+++ src/event/keyboard.js
@@ -1,5 +1,19 @@
 import Keyboard from 'pixi.js-keyboard';
 
+const PlayerIsRun = (player, player_base) => {
+  if(player.texture_actually !== "run") {
+    player.textures = player_base.texture.run;
+    player.texture_actually = "run";
+    if(!player.playing) player.play();
+  } 
+}
+
+const PlayerIsStop = (player, player_base) => {
+  if(player.texture_actually !== "stand") {
+    player.textures = player_base.texture.stand;
+  }
+}
+
 export const PlayerKeyboardListener = (delta, player, player_base) => {
   let stop = true;
   
@@ -7,11 +21,7 @@ export const PlayerKeyboardListener = (delta, player, player_base) => {
     stop = false;
     player.anchor.x = 0.75; 
     player.scale.x = -2.5;
-    if(player.texture_actually !== "run") {
-      player.textures = player_base.texture.run;
-      player.texture_actually = "run";
-      if(!player.playing) player.play();
-    } 
+    PlayerIsRun(player, player_base);
     if(player.x <= 0) return;
     player.x -= player.vx * delta;
   }
@@ -20,41 +30,27 @@ export const PlayerKeyboardListener = (delta, player, player_base) => {
     stop = false;
     player.anchor.x = 0; 
     player.scale.x = 2.5;
-    if(player.texture_actually !== "run") {
-      player.textures = player_base.texture.run;
-      player.texture_actually = "run";
-      if(!player.playing) player.play();
-    } 
+    PlayerIsRun(player, player_base);
     if(player.x >= 63 * 150) return;
     player.x += player.vx * delta;
   }
 
   if (Keyboard.isKeyDown('ArrowUp', 'KeyW')) {
     stop = false;
-    if(player.texture_actually !== "run") {
-      player.textures = player_base.texture.run;
-      player.texture_actually = "run";
-      if(!player.playing) player.play();
-    } 
+    PlayerIsRun(player, player_base);
     if(player.y <= 0) return;
     player.y -= player.vy * delta;
   }
 
   if (Keyboard.isKeyDown('ArrowDown', 'KeyS')) {
     stop = false;
-    if(player.texture_actually !== "run") {
-      player.textures = player_base.texture.run;
-      player.texture_actually = "run";
-      if(!player.playing) player.play();
-    } 
+    PlayerIsRun(player, player_base);
     if(player.y >= 63 * 150) return;
     player.y += player.vy * delta;
   }
 
   if(stop) {
-    if(player.texture_actually !== "stand") {
-      player.textures = player_base.texture.stand;
-    }
+    PlayerIsStop(player, player_base);
     player.texture_actually = "stand";
     if(!player.playing) player.play();
   } 
