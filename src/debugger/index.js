407bff2072fc5b33517b2e5496a3901134384b41
Novout <novout@hotmail.com>
2021-02-18 06:55:37 -0300

    fix(linter): debugger

--- src/debugger/index.js
+++ src/debugger/index.js
@@ -1,18 +1,21 @@
 import { ticker } from '@/pixi/alias';
 import { CreateUIDebugger } from '@/ui/debugger';
 
-export const Warning = () => {}
-export const Error = () => {}
-export const Success = () => {}
+export const Warning = () => {};
+export const Error = () => {};
+export const Success = () => {};
 
 export const Create = (stage, player) => {
   const ui = CreateUIDebugger(stage, player);
   return ui;
-}
+};
 
 export const RenderFrameRate = (ui, player, renderer) => {
-  ui.position.set(player.x + renderer.screen.width / 4, player.y - renderer.screen.height / 4);
+  ui.position.set(
+    player.x + renderer.screen.width / 4,
+    player.y - renderer.screen.height / 4
+  );
 
-  const _framerate = ticker.shared.FPS
+  const _framerate = ticker.shared.FPS;
   ui.framerate.text = `FPS: ${ticker.shared.FPS.toFixed(1)}`;
-}
+};
--- src/ui/debugger.js
+++ src/ui/debugger.js
@@ -2,13 +2,13 @@ import { KContainer, KText } from './material';
 
 export const CreateUIDebugger = (stage, player) => {
   const context = KContainer(stage, { x: player.x, y: player.y + 200 });
-  const _framerate = KText(
-    0,
-    context,
-    { fontFamily: 'KitchenSink', fontSize: 24, fill: 0xffffff },
-  );
+  const _framerate = KText(0, context, {
+    fontFamily: 'KitchenSink',
+    fontSize: 24,
+    fill: 0xffffff
+  });
 
   context.framerate = _framerate;
 
   return context;
-}
+};
