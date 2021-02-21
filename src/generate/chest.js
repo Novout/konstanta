ee7e5523d48512ea8862d8712273523aeb458053
Novout <novout@hotmail.com>
2021-02-20 21:23:09 -0300

    feat(ui): chest drop item

--- src/generate/chest.js
+++ src/generate/chest.js
@@ -2,12 +2,18 @@ import ITEMS from '@/defines/items.json';
 import * as Debugger from '@/debugger';
 
 export const getGenerateItem = (player) => {
-  const _items = ITEMS.filter((item) => player.level === item.id_cont);
+  const _items = ITEMS.filter((item) => player.level == item.id_cont && !item.type_tags.includes('unknown'));
   const _random = Math.floor(Math.random() * _items.length);
 
   const _item = _items[_random];
+  if (!_item) Debugger.ThrowError('Item not exists in generate.');
 
-  if (_item) Debugger.ThrowError('Item not exists in generate');
+  _item.colorBackground = ({
+    "common": 0xC6C6C6,
+    "uncommon": 0x84DAFF,
+    "epic": 0xB884FF,
+    "legendary": 0xFF8C4B
+  }[_item.rarity] || "")
 
   return _item;
 };
--- src/ui/chests.js
+++ src/ui/chests.js
@@ -2,6 +2,7 @@ import { Sprite } from '@/pixi/alias';
 import { UIAlpha, InterfaceGlow, FXAA } from '@/utils/webgl';
 import { KText, KContainer, KGraphics } from './material';
 import { getAltarChoices } from '@/generate/altar';
+import { getGenerateItem } from '@/generate/chest';
 import { setPlayerSkill } from '@/interceptor/player';
 
 export const CreateChestButton = (app, player, item, resources) => {
@@ -32,16 +33,82 @@ export const CreateChestButton = (app, player, item, resources) => {
     }
   );
 
-  button.on('click', () => {});
+  button.on('click', () => {
+    const _item = getGenerateItem(player);
+
+    const item_container = KGraphics(main, {
+      fill: 0x66bd99,
+      rectangle: [0, 0, 256, 512],
+    }, {
+      x: 0,
+      y: -(app.renderer.screen.height / 2)
+    })
+
+    const item_container_center = KGraphics(item_container, {
+      fill: _item.colorBackground,
+      rectangle: [0, 0, 256, 512],
+      filters: [InterfaceGlow(_item.colorBackground)]
+    })
+
+    const item_container_title = KText(
+      _item.title,
+      item_container_center,
+      {
+        fontFamily: 'KitchenSink',
+        fontSize: 22,
+        fontWeight: 'bold'
+      },
+      {
+        positional: { x: 0, y: item_container_center.height / 10 }
+      }
+    );
+
+    const item_container_pick = KText(
+      'Pegar Item',
+      item_container_center,
+      {
+        fontFamily: 'KitchenSink',
+        fontSize: 20,
+        fontWeight: 'bold'
+      },
+      {
+        button: true,
+        filters: [FXAA()],
+        positional: { x: item_container_center.width / 10, y: item_container_center.height - 80 }
+      }
+    );
+    item_container_pick.on('click', () => {
+      DeleteChestButton(item, item_container);
+    });
+
+    const item_container_discart = KText(
+      'Descartar Item',
+      item_container_center,
+      {
+        fontFamily: 'KitchenSink',
+        fontSize: 20,
+        fontWeight: 'bold'
+      },
+      {
+        button: true,
+        filters: [FXAA()],
+        positional: { x: item_container_center.width / 10, y: item_container_center.height - 40 }
+      }
+    );
+    item_container_discart.on('click', () => {
+      DeleteChestButton(item, item_container);
+    });
+  });
 
   main.visible = false;
 
   item.ui_chest = main;
+  item.ui_chest_button = container;
 
   return main;
 };
 
-export const DeleteAltarButton = (item, main) => {
+export const DeleteChestButton = (item, main) => {
   item.visible = false;
   main.visible = false;
   //main.destroy(true);
