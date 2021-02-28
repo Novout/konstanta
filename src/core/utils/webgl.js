7694f739a7b388b9fb56d53066d0ea653890a52d
Novout <novout@hotmail.com>
2021-02-27 22:49:33 -0300

    docs: set commands

--- src/core/utils/webgl.js
+++ src/core/utils/webgl.js
@@ -2,6 +2,12 @@ import { utils, filters } from '@/pixi/alias';
 import { GlowFilter } from 'pixi-filters';
 import * as Debugger from '@/debugger';
 
+/**
+ * Objeto de Filtros do Pixi-Filters para deixar itens transparentes.
+ * 
+ * Pixi-Filters object to make items transparent.
+ * @returns {Array<Filters>} Lista de Filtros do Pixi-Filters
+ */
 export const UIAlpha = () => {
   const alpha_bar = new filters.AlphaFilter(0.8);
   const alpha_main = new filters.AlphaFilter(0.6);
@@ -10,8 +16,20 @@ export const UIAlpha = () => {
   return { alpha_bar, alpha_main, alpha_item };
 };
 
+/**
+ * Filtro do Pixi-Filters para habilitar FXAA no objeto anexado.
+ * 
+ * Pixi-Filters filter to enable FXAA no attached objects.
+ * @returns {Filter} Filtros do Pixi-Filters
+ */
 export const FXAA = () => (new filters.FXAAFilter().enabled = true);
 
+/**
+ * Filtro do Pixi-Filters para emitir iluminação externa.
+ * 
+ * Pixi-Filters filter to emit external lighting.
+ * @param {number} [_color=0x66bd99] HEX Color
+ */
 export const InterfaceGlow = (_color = 0x66bd99) =>
   new GlowFilter({
     distance: 25,
@@ -21,6 +39,11 @@ export const InterfaceGlow = (_color = 0x66bd99) =>
     knockout: false
   });
 
+/**
+ * Apenas renderiza o canvas se o navegador tiver WEBGL.
+ * 
+ * Only render the canvas if the browser has WEBGL.
+ */
 export const OnlyWEBGL = () => {
   if (!utils.isWebGLSupported()) {
     Debugger.ThrowError('WEBGL is required!');
