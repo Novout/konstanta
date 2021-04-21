2b3b178efd50088a5d6fc1bb9eff9922f4ecdd51
Novout <novout@hotmail.com>
2021-04-21 00:14:16 -0300

    feat(loader): bar in background added

--- src/pages/loader.js
+++ src/pages/loader.js
@@ -7,6 +7,9 @@ export const useLoader = () => {
     if(_container) {
       const _text = document.getElementById('__LOADER_TEXT');
       _text.textContent = `${content.progress}%`;
+
+      const _bar = document.getElementById('__LOADER_BAR_CONTENT');
+      _bar.style.width = `${content.progress}%`;
     } else {
       const background = document.createElement('div');
       background.id = "__LOADER_FOG";
@@ -26,8 +29,24 @@ export const useLoader = () => {
       percentage.id = "__LOADER_TEXT";
       percentage.textContent = `${content.progress}%`;
 
+      const bar = document.createElement('div');
+      bar.id = "__LOADER_BAR";
+      bar.style.width = "80%";
+      bar.style.height = "1.5rem";
+      bar.style.border = "1px solid";
+      bar.style.borderColor = "black";
+      
+      const _bar = document.createElement('div');
+      _bar.id = "__LOADER_BAR_CONTENT";
+      _bar.style.width = `${content.progress}%`;
+      _bar.style.height = "1.5rem";
+      _bar.style.backgroundColor = "#66bd99";
+
+      bar.appendChild(_bar);
+
       container.appendChild(title);
       container.appendChild(percentage);
+      container.appendChild(bar);
 
       document.body.appendChild(container);
       document.body.appendChild(background);
